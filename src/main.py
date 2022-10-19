ad5afec0f6add2616c22faf744737cede530e555
dharmilshah99 <dharmil.shah@hotmail.com>
2022-10-19 16:35:11 +0100

    Some Changes

--- src/main.py
+++ src/main.py
@@ -8,12 +8,32 @@ from numpy.random import default_rng
 
 
 class Node:
+    """Represents a Decision Tree Node."""
+
     def __init__(self, left=None, right=None, attribute=None, value=None, label=None):
+        """Initializes Decision Tree Node.
+
+        Args:
+            left (Node, optional): Node left of current node. Defaults to None.
+            right (Node, optional): Node right of current node. Defaults to None.
+            attribute (int, optional): Attribute to split dataset upon. Defaults to None.
+            value (float, optional): Split point value. Defaults to None.
+            label (int, optional): Classification label of Node. Defaults to None.
+        """
         self.left = left
         self.right = right
         self.attribute = attribute
         self.value = value
         self.label = label
+
+    def is_root(self):
+        """Checks if Node is a root.
+
+        Returns:
+            True if Node has children. False, otherwise.
+        """
+        return (self.left == None) and (self.right == None)
+
 
 ###
 # Helpers
@@ -152,23 +172,22 @@ def decision_tree_learning(dataset, depth):
 
         return Node(left, right, attribute, split), max(l_depth, r_depth)
 
-# TODO: verify + complete documentation
+
 def predict(decision_tree, x):
-    """Performs prediction on some samples using a decision tree
+    """Performs prediction on some samples using a decision tree.
 
     Args:
-        decision_tree (Node): Decision tree 
-        x (np.ndarray): Instances, numpy array with shape (N,K)
+        decision_tree (Node): Decision tree.
+        x (np.ndarray): Instances, numpy array with shape (N,K).
 
     Returns:
-        y (np.ndarray): Predicted class labels, numpy array with shape (N,)
+        y (np.ndarray): Predicted class labels, numpy array with shape (N,).
     """
     y = np.zeros(len(x))
     for idx, inst in enumerate(x):
         # Traverse tree until leaf
         curr_node = decision_tree
-        while curr_node.label == None:
-            # assert(inst[:, [curr_node.attribute]])
+        while not curr_node.is_root():
             if inst[curr_node.attribute] < curr_node.value:
                 curr_node = curr_node.left
             else:
@@ -176,22 +195,19 @@ def predict(decision_tree, x):
         y[idx] = curr_node.label
     return y
 
+
 def split_dataset(dataset, test_proportion, random_generator=default_rng()):
-    """ Split dataset into training and test sets, according to the given 
-        test set proportion.
-    
+    """ Split dataset into training and test sets, according to the given test set proportion.
+
     Args:
-        dataset (np.ndarray): Instances, numpy array with shape (N,K)
-        x (np.ndarray): Instances, numpy array with shape (N,K)
-        y (np.ndarray): Class labels, numpy array with shape (N,)
-        test_proprotion (float): the desired proportion of test examples 
-                                 (0.0-1.0)
+        dataset (np.ndarray): Instances, numpy array with shape (N,K+1).
+        test_proprotion (float): the desired proportion of test examples (0.0-1.0)
         random_generator (np.random.Generator): A random generator
 
     Returns:
-        tuple: returns a tuple of (x_train, x_test, y_train, y_test) 
-               - train_dataset (np.ndarray): Training dataset shape (N_train, K)
-               - test_dataset (np.ndarray): Test instances shape (N_test, K)
+        tuple: returns a tuple of (train_dataset, test_dataset) 
+            - train_dataset (np.ndarray): Training dataset shape (N_train, K+1)
+            - test_dataset (np.ndarray): Test instances shape (N_test, K+1)
     """
 
     shuffled_indices = random_generator.permutation(len(dataset))
@@ -201,37 +217,40 @@ def split_dataset(dataset, test_proportion, random_generator=default_rng()):
     test_dataset = dataset[shuffled_indices[n_train:]]
     return (train_dataset, test_dataset)
 
+
 def compute_accuracy(y_gold, y_prediction):
-    """ Compute the accuracy given the ground truth and predictions
+    """Compute the accuracy given the ground truth and predictions.
 
     Args:
-    y_gold (np.ndarray): the correct ground truth/gold standard labels
-    y_prediction (np.ndarray): the predicted labels
+        y_gold (np.ndarray): the correct ground truth/gold standard labels.
+        y_prediction (np.ndarray): the predicted labels.
 
     Returns:
-    float : the accuracy
+        float : the accuracy.
     """
 
-    assert len(y_gold) == len(y_prediction)  
-    
+    assert len(y_gold) == len(y_prediction)
+
     try:
         return np.sum(y_gold == y_prediction) / len(y_gold)
     except ZeroDivisionError:
         return 0
+
 ###
 # Main
 ###
 
+
 if __name__ == "__main__":
     # Parse
-    path = "wifi_db/noisy_dataset.txt"
+    path = "wifi_db/clean_dataset.txt"
     x, y, dataset = read_dataset(path)
 
     seed = 60012
     rg = default_rng(seed)
-    train_dataset, test_dataset = split_dataset(dataset, 
-                                                test_proportion=0.2, 
-                                                random_generator=rg)
+    train_dataset, test_dataset = split_dataset(
+        dataset, test_proportion=0.2, random_generator=rg
+    )
     print(train_dataset.shape)
     print(test_dataset.shape)
 
