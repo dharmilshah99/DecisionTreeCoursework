06ae41cb11b54879ea1ba917c3881a3485eb0db7
dharmilshah99 <dharmil.shah@hotmail.com>
2022-10-25 11:10:09 +0100

    Add confusion matrix

--- src/eval.py
+++ src/eval.py
@@ -29,11 +29,11 @@ def compute_accuracy(y_gold, y_prediction):
     """Compute the accuracy given the ground truth and predictions.
 
     Args:
-        y_gold (np.ndarray): the correct ground truth/gold standard labels.
-        y_prediction (np.ndarray): the predicted labels.
+        y_gold (np.ndarray): Ground truth/Gold standard labels.
+        y_prediction (np.ndarray): Predicted labels.
 
     Returns:
-        float : the accuracy.
+        float: the accuracy.
     """
 
     assert len(y_gold) == len(y_prediction)
@@ -42,6 +42,29 @@ def compute_accuracy(y_gold, y_prediction):
         return np.sum(y_gold == y_prediction) / len(y_gold)
     except ZeroDivisionError:
         return 0
+
+def generate_confusion_matrix(y_gold, y_prediction):
+    """Generates a confusion matrix given ground truth and predictions
+
+    Args:
+        y_gold (np.ndarray): Ground truth/Gold Standard labels.
+        y_predictions (np.ndarray): Predicted labels.
+    
+    Returns:
+        confusion_matrix (np.ndarray): A 4 by 4 confusion matrix.
+    """
+
+    assert len(y_gold) == len(y_prediction)
+
+    # Match labels to confusion matrix indexes.
+    y_gold, y_prediction = y_gold - 1, y_prediction - 1
+    
+    # Populate Confusion Matrix
+    confusion_matrix = np.zeros((4, 4), dtype=np.int32) # Number of rooms is 4.
+    for gold, prediction in zip(y_gold, y_prediction):
+        confusion_matrix[gold][prediction] += 1
+
+    return confusion_matrix
 
 if __name__ == "__main__":
     pass
