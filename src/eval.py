fadb302bfb75a49cc41491ac33bf903429e88b83
dharmilshah99 <dharmil.shah@hotmail.com>
2022-10-25 17:42:28 +0100

    Format

--- src/eval.py
+++ src/eval.py
@@ -31,7 +31,7 @@ def generate_confusion_matrix(y_gold, y_prediction):
     Args:
         y_gold (np.ndarray): Ground truth/Gold Standard labels.
         y_predictions (np.ndarray): Predicted labels.
-    
+
     Returns:
         confusion_matrix (np.ndarray): A 4 by 4 confusion matrix.
     """
@@ -64,27 +64,28 @@ def compute_accuracy(confusion_matrix):
     except ZeroDivisionError:
         return 0
 
+
 def compute_precision(confusion_matrix):
-    """ Compute the precision score per class given the ground truth and predictions
-        
+    """Compute the precision score per class given the ground truth and predictions
+
     Also return the macro-averaged precision across classes.
-        
+
     Args:
         y_gold (np.ndarray): the correct ground truth/gold standard labels
         y_prediction (np.ndarray): the predicted labels
 
     Returns:
         tuple: returns a tuple (precisions, macro_precision) where
-            - precisions is a np.ndarray of shape (C,), where each element is the 
+            - precisions is a np.ndarray of shape (C,), where each element is the
               precision for class c
-            - macro-precision is macro-averaged precision (a float) 
+            - macro-precision is macro-averaged precision (a float)
     """
-    p = np.zeros((len(confusion_matrix), ))
-    
+    p = np.zeros((len(confusion_matrix),))
+
     for i in range(len(confusion_matrix)):
         if np.sum(confusion_matrix[:, i]) > 0:
-            p = confusion_matrix[i][i]/np.sum(confusion_matrix[:, i])
-    
+            p = confusion_matrix[i][i] / np.sum(confusion_matrix[:, i])
+
     # Compute the macro-averaged precision
     macro_p = 0
     if len(p) > 0:
@@ -93,27 +94,27 @@ def compute_precision(confusion_matrix):
 
 
 def compute_recall(confusion_matrix):
-    """ Compute the recall score per class given the ground truth and predictions
-        
+    """Compute the recall score per class given the ground truth and predictions
+
     Also return the macro-averaged recall across classes.
-        
+
     Args:
         y_gold (np.ndarray): the correct ground truth/gold standard labels
         y_prediction (np.ndarray): the predicted labels
 
     Returns:
         tuple: returns a tuple (recalls, macro_recall) where
-            - recalls is a np.ndarray of shape (C,), where each element is the 
+            - recalls is a np.ndarray of shape (C,), where each element is the
                 recall for class c
-            - macro-recall is macro-averaged recall (a float) 
+            - macro-recall is macro-averaged recall (a float)
     """
-    r = np.zeros((len(confusion_matrix), ))
-    
+    r = np.zeros((len(confusion_matrix),))
+
     for i in range(len(confusion_matrix)):
         if np.sum(confusion_matrix[i, :]) > 0:
-            r = confusion_matrix[i][i]/np.sum(confusion_matrix[i,: ])
-    
-    # Compute the macro-averaged precision
+            r = confusion_matrix[i][i] / np.sum(confusion_matrix[i, :])
+
+    # Compute the macro-averaged recall
     macro_r = 0
     if len(r) > 0:
         macro_r = np.mean(r)
@@ -121,19 +122,19 @@ def compute_recall(confusion_matrix):
 
 
 def compute_f1_score(y_gold, y_prediction):
-    """ Compute the F1-score per class given the ground truth and predictions
-        
+    """Compute the F1-score per class given the ground truth and predictions
+
     Also return the macro-averaged F1-score across classes.
-        
+
     Args:
         y_gold (np.ndarray): the correct ground truth/gold standard labels
         y_prediction (np.ndarray): the predicted labels
 
     Returns:
         tuple: returns a tuple (f1s, macro_f1) where
-            - f1s is a np.ndarray of shape (C,), where each element is the 
+            - f1s is a np.ndarray of shape (C,), where each element is the
               f1-score for class c
-            - macro-f1 is macro-averaged f1-score (a float) 
+            - macro-f1 is macro-averaged f1-score (a float)
     """
 
     (precisions, macro_p) = compute_precision(y_gold, y_prediction)
@@ -143,12 +144,15 @@ def compute_f1_score(y_gold, y_prediction):
 
     # Just to make sure they are of the same length
     assert len(precisions) == len(recalls)
-    f = np.zeros((len(precisions), )) 
-    f = (2*precisions * recalls / (precisions +recalls))
+
+    f = np.zeros((len(precisions),))
+    f = 2 * precisions * recalls / (precisions + recalls)
+
     macro_f = 0
     if len(f) > 0:
         macro_f = np.mean(f)
     return (f, macro_f)
+
 
 if __name__ == "__main__":
     pass
